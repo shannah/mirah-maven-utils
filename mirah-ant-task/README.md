19a0053712e4883328cbcb51f83e870d9cccde66
shannah <steve@weblite.ca>
2014-11-04 14:08:14 -0800

    Updated MirahcTask with license info.

---
+++ mirah-ant-task/README.md
@@ -0,0 +1,11 @@
+#Mirah ANT Task
+
+This project includes an ANT task for building Java projects that include Mirah sources.
+
+##License
+
+Apache 2.0
+
+##Usage
+
+
--- mirah-ant-task/src/main/java/ca/weblite/mirah/ant/MirahcTask.java
+++ mirah-ant-task/src/main/java/ca/weblite/mirah/ant/MirahcTask.java
@@ -1,9 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2014 Steve Hannah
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package ca.weblite.mirah.ant;
 
 // IMPORTANT! You need to compile this class against ant.jar.
@@ -117,14 +126,17 @@ public class MirahcTask extends Task {
                     }
                     
                 });
-                for ( File jar : jars ){
-                    macroClassPath.add(new Path(getProject(), jar.getAbsolutePath()));
+                if ( jars != null ){
+                    for ( File jar : jars ){
+                        macroClassPath.add(new Path(getProject(), jar.getAbsolutePath()));
+                    }
                 }
             }
         }
     
         
         if ( getMacroClassPath() != null ){
+            System.out.println("Setting macro classpath to "+getMacroClassPath().toString());
             c.setMacroClassPath(getMacroClassPath().toString());
         }
         if ( getDest() != null ){
